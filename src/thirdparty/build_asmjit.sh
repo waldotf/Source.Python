317b46e5ebe8587efb40fd263328d3e96ddb07a7
waldotf <waldo.waldotf2@gmail.com>
2025-04-10 18:51:20 -0700

    Different filename for static build

--- src/thirdparty/build_asmjit.sh
+++ src/thirdparty/build_asmjit.sh
@@ -10,7 +10,7 @@ make all
 # Export libs
 cd ../../..
 mkdir -p libs/AsmJit/
-cp AsmJit/build/Release/libasmjit.so libs/AsmJit/
+cp AsmJit/build/Release/libasmjit.a libs/AsmJit/
 mkdir -p include/AsmJit/
 cp -r AsmJit/src/asmjit/* include/AsmJit/
 # Prune cpp files
@@ -19,6 +19,6 @@ find include/AsmJit/ -name *.cpp | xargs rm
 # Also copy to DynamicHooks submodule since we build that next
 rm -rf DynamicHooks/src/thirdparty/AsmJit/include/*
 mkdir -p DynamicHooks/src/thirdparty/AsmJit/lib
-cp libs/AsmJit/libasmjit.so DynamicHooks/src/thirdparty/AsmJit/lib/
+cp libs/AsmJit/libasmjit.a DynamicHooks/src/thirdparty/AsmJit/lib/
 mkdir -p DynamicHooks/src/thirdparty/AsmJit/include
 cp -r include/AsmJit/* DynamicHooks/src/thirdparty/AsmJit/include/
